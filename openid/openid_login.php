eed0cbd08f6b8e384fae47fdeea7e2b0a82ae266
Arun Scaria <arunscaria91@gmail.com>
2012-06-13 22:06:12 +0530

    Disable openid on masterlist enabled

--- openid/openid_login.php
+++ openid/openid_login.php
@@ -1,16 +1,42 @@
 <?php
+/* * ******************************************************************** */
+/* ATutor							          */
+/* * ******************************************************************** */
+/* Copyright (c) 2002-2012                                                */
+/* Inclusive Design Institute	                                          */
+/* http://atutor.ca                                                       */
+/*                                      			          */
+/* This program is free software. You can redistribute it and/or          */
+/* modify it under the terms of the GNU General Public License            */
+/* as published by the Free Software Foundation.                          */
+/* * ******************************************************************** */
+//$Id: openid_login.php UTF-8 10055 Jun 13, 2012 7:08:49 PM Author:scari   $
 
 $_user_location	= 'public';
 
 define('AT_INCLUDE_PATH', '../../include/');
 require (AT_INCLUDE_PATH.'vitals.inc.php');
+require ('openid_util_fns.php');
 $_custom_css = $_base_path . 'mods/openid/module.css'; // use a custom stylesheet
+
+#Unset the session first.
+unset_session();
 require (AT_INCLUDE_PATH.'header.inc.php');
 ?>
-<div class="openid_input-form" id="opnid_wraper">
-	<p id="openid_text">Login to ATutor with any of these accounts.</p>
+<div class="openid_input-form" id="opnid_wraper">   
+	<p id="openid_text"></p>
 <div id="openid_vertical_divider">
 	<div id="openid_indicator_arrow"></div>
+        <?php 
+            #Disable openid if master list is enabled.
+            if (defined('AT_MASTER_LIST') && AT_MASTER_LIST) {
+        ?> 
+            <p id="openid_content_text" style="margin-top: 50px; width: 90%;">The masterlist is enabled. OpenID module can't work properly with masterlist. 
+                Contact the admin to disable this functionality at <a href="admin/config_edit.php">System Preferences</a>.
+            </p>
+        <?php
+            }else{
+        ?>
 	<div id="openid_login-btns-wrapper">
 		<fieldset>
 			<span style="padding-left: 20px;">Click to login</span>
@@ -21,6 +47,7 @@ require (AT_INCLUDE_PATH.'header.inc.php');
 			<li><a href="mods/openid/twitter_login" class="openid_one-click-login openid_social_label" id="openid_twitter">Login with Twitter</a></li>
 		  
 	</div>
+        <?php } ?>
 </div>
 </div>
 
