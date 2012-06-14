d8130a9c0a119db60ab66e977aed7c589a271cf3
Arun Scaria <arunscaria91@gmail.com>
2012-06-15 02:05:27 +0530

    added openid vital and confiured OP

--- openid/openid_login.php
+++ openid/openid_login.php
@@ -16,11 +16,12 @@ $_user_location	= 'public';
 
 define('AT_INCLUDE_PATH', '../../include/');
 require (AT_INCLUDE_PATH.'vitals.inc.php');
-require ('openid_util_fns.php');
-$_custom_css = $_base_path . 'mods/openid/module.css'; // use a custom stylesheet
 
+require ('openid.vitals.inc.php');
 #Unset the session first.
-unset_session();
+unsetSession();
+
+$_custom_css = $_base_path . 'mods/openid/module.css'; // use a custom stylesheet
 require (AT_INCLUDE_PATH.'header.inc.php');
 ?>
 <div class="openid_input-form" id="opnid_wraper">   
@@ -42,13 +43,18 @@ require (AT_INCLUDE_PATH.'header.inc.php');
 			<span style="padding-left: 20px;">Click to login</span>
 		</fieldset>
 		   	
-			<li><a href="mods/openid/google_login/login.php?login=true&openid_provider=google" class="openid_one-click-login openid_social_label" id="openid_google">Login with Google</a></li>
-			<li><a href="mods/openid/facebook_login" class="openid_one-click-login openid_social_label" id="openid_facebook">Login with Facebook</a></li>
-			<li><a href="mods/openid/twitter_login" class="openid_one-click-login openid_social_label" id="openid_twitter">Login with Twitter</a></li>
+			<?php 
+                              if($_openid_config['OPENID_GOOGLE_ENABLED'] == 'true')  
+                                    echo '<li><a href="mods/openid/google_login/login.php?login=true&openid_provider=google" class="openid_one-click-login openid_social_label" id="openid_google">Login with Google</a></li>'; 
+                              if($_openid_config['OPENID_FB_ENABLED'] == 'true') 
+                                    echo '<li><a href="mods/openid/facebook_login" class="openid_one-click-login openid_social_label" id="openid_facebook">Login with Facebook</a></li>';
+                              if($_openid_config['OPENID_TWITTER_ENABLED'] == 'true') 
+                                    echo '<li><a href="mods/openid/twitter_login" class="openid_one-click-login openid_social_label" id="openid_twitter">Login with Twitter</a></li>'; 
+                        ?>
 		  
 	</div>
         <?php } ?>
 </div>
 </div>
 
-<?php require (AT_INCLUDE_PATH.'footer.inc.php'); ?>
+<?php require (AT_INCLUDE_PATH.'footer.inc.php'); ?>                                                                                                                                                                                                                                  
