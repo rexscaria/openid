5e8f6031ea527c5547ad556a272872de74ff9e4a
Arun Scaria <arunscaria91@gmail.com>
2012-06-28 23:53:32 +0530

    added oauth support

--- openid/google/login.php
+++ openid/google/login.php
@@ -1,5 +1,4 @@
 <?php
-
 /* * ******************************************************************** */
 /* ATutor							          */
 /* * ******************************************************************** */
@@ -18,14 +17,16 @@ $_user_location	= 'public';
 define('AT_INCLUDE_PATH', '../../../include/');
 require (AT_INCLUDE_PATH.'vitals.inc.php');
 #importing required files
-require ('openid_utility.php');
-require ('../openid.vitals.inc.php');
+require_once  ('libs/google_openid_consts.php');
+require_once  ('libs/openid_utility.php');
+require_once  ('../openid.vitals.inc.php');
 
 #callback URL
 define('CALLBACK_URL',"http://".$_SERVER['SERVER_NAME'].AT_BASE_HREF."mods/openid/login.php" );
 define('GOOGLE_IDENTITY_URL','https://www.google.com/accounts/o8/id');
 define('OPENID_LOGIN_PAGE_URL',AT_BASE_HREF.'mods/openid/openid_login.php');
 define('OPENID_MOD_DIR', AT_BASE_HREF.'mods/openid/');
+define('OPENID_PROVIDER','GOOGLE');
 
 #Disable openid if master list is enabled.
 if (defined('AT_MASTER_LIST') && AT_MASTER_LIST) {
@@ -46,15 +47,25 @@ if (defined('AT_MASTER_LIST') && AT_MASTER_LIST) {
                   #setting call back url
                   //$openid->returnUrl = CALLBACK_URL;
                   #Select openid mode as 'checkid_setup'
-                  $immediate_mode = ($_openid_config['OPENID_GOOGLE_MODE'] == 'checkid_immediate')?  false : true;
+                  $immediate_mode = ($_openid_config['OPENID_GOOGLE_MODE'] == 'checkid_immediate')?  true : false;
                   #Set the required ax params.
                   $openid->required = array(
                         'email'       =>   'contact/email',
                         'firstname'   =>   'namePerson/first',
                         'lastname'    =>   'namePerson/last',
-                        /*'country'     =>   'contact/country/home',
+                      /*'country'     =>   'contact/country/home',
                         'language'    =>   'pref/language'*/
                       );
+                  
+                  #Should we query the country.
+                  if($_openid_config['OPENID_GOOGLE_QUERY_COUNTRY']=='true'){
+                      $openid->required['country'] = 'contact/country/home';
+                  }
+                  
+                  #Or lanugage?
+                  if($_openid_config['OPENID_GOOGLE_QUERY_LANGUAGE']=='true'){
+                      $openid->required['country'] = 'pref/language';
+                  }
                                 
                   #Set UI params
                   if($_openid_config['OPENID_GOOGLE_ENABLE_UI_MODE'] != 'false'){
@@ -75,27 +86,26 @@ if (defined('AT_MASTER_LIST') && AT_MASTER_LIST) {
                   /*
                    * 
                    * The OAuth is inactive now. But may active in future.
-                   * 
+                   */
                   #Set ext2(OAuth 2.0) params
                   $oauth_str = '';
                   if($_openid_config['OPENID_GOOGLE_REQUEST_OAUTH'] == 'true' 
                           && $_openid_config['OPENID_GOOGLE_OAUTH_CONSUMER_KEY'] != ''){
                       #TODO: Add scope.
-                      $oauthNs = urlencode("http://specs.openid.net/extensions/oauth/1.0");
-                      $scope = urlencode('http://gdata.youtube.com'.' '.
-                               'https://docs.google.com/feeds/'.' '.
-                               'https://www.google.com/calendar/feeds/');
-                      $oauth_str = '&openid.ns.ext2='.$oauthNs.
-                                             '&openid.ext2.consumer='.urlencode($_openid_config['OPENID_GOOGLE_OAUTH_CONSUMER_KEY']).
-                                             '&openid.ext2.scope='.$scope;
-                  }*/
+                      $scopes = array(
+                          OAUTH_SCOPE_GOOGLE_YOUTUBE,
+			  OAUTH_SCOPE_GOOGLE_DOCS,
+		 	  OAUTH_SCOPE_GOOGLE_CALENDAR,                         
+                      );
+                      $oauth_str = $openid->createOAuthURL($scopes, $_openid_config['OPENID_GOOGLE_OAUTH_CONSUMER_KEY']);
+                  }
                                 
                   /*
                   * Now everything is set. Find the end point with OpenID
                   * discovery and redirect to the authentication url,
                   */
                   
-                  header('Location: ' . $openid->authUrl() .$oauth_str);
+                  header('Location: ' . $openid->authUrl($immediate_mode) .$oauth_str);
                   exit;
          }
          else if($openid->mode=='cancel'){
@@ -127,181 +137,10 @@ if (defined('AT_MASTER_LIST') && AT_MASTER_LIST) {
                  
                  if(mysql_num_rows($result)==0){
                      #Email doesn't exist in DB. Register the user.
-                                         
-                     #Is registration allowed??
-                     if($_config['allow_registration'] != 1){
-                         $msg->addInfo('REG_DISABLED');
-                         header('Location: '.OPENID_LOGIN_PAGE_URL);
-                         exit;
-                     }
-                     
-                                          
-                     #No Email conformation is needed for OpenID login.
-                     $status = AT_STATUS_STUDENT;
-                     $now = date('Y-m-d H:i:s');
-                     $default_private_email = 1;
-                     
-                     #Its time to select a login name.
-                     $login_name = getFreeLoginName($db, $openid_fname, $openid_lname,$openid_email);
-
-                     $sql = "INSERT INTO ".TABLE_PREFIX."members 
-		              (login,
-		               email,
-		               first_name,
-		               last_name,
-		               country,
-		               status,
-                               preferences,
-		               creation_date,
-		               language,
-		               inbox_notify,
-		               private_email,
-		               last_login)
-		       VALUES ('$login_name',
-		               '$openid_email',
-		               '$openid_fname',
-                               '$openid_lname',
-		               '$openid_country', 
-		                $status, 
-		               '$_config[pref_defaults]', 
-		               '$now',
-		               '$_SESSION[lang]', 
-		                $_config[pref_inbox_notify], 
-		                $default_private_email, 
-		               '0000-00-00 00:00:00')";
-                     
-                     $result = mysql_query($sql, $db) or die(mysql_error());
-                     $m_id   = mysql_insert_id($db);
-                     if (!$result) {
-			 $msg->addError('DB_NOT_UPDATED');
-			 header('Location: '.OPENID_LOGIN_PAGE_URL);
-                         exit;
-                     }else{
-                         #Reset login attempts.
-                         $sql = "DELETE FROM ".TABLE_PREFIX."member_login_attempt WHERE login='$login_name'";
-			 mysql_query($sql, $db);
- 
-                         #If en_id is set, automatically enroll into courses that links with en_id and go to "My Start Page"
-			 $member_id = $m_id;
-                         $default_course_id = 0;
-
-                         require (AT_INCLUDE_PATH.'html/auto_enroll_courses.inc.php');
-			
-			 #Update last_login
-			 $sql = "UPDATE ".TABLE_PREFIX."members 
-			           SET last_login=now(), creation_date=creation_date 
-			         WHERE member_id=".$member_id;
-			 mysql_query($sql, $db);
-			
-			 #Write to session.
-			 $_SESSION['valid_user'] = true;
-			 $_SESSION['member_id']	= $m_id;
-			 $_SESSION['course_id']  = 0;
-			 $_SESSION['login'] = $login_name;
-			 assign_session_prefs(unserialize(stripslashes($_config[pref_defaults])), 1);
-			 $_SESSION['is_guest']	= 0;
-			 $_SESSION['lang'] = $_SESSION[lang];
-                         $_SESSION['first_login'] = true;
-                         
-                         #Its an OpenID session
-                         $_SESSION['is_openid'] = true;
-                         $_SESSION['openid_claimed_id'] = $openid->identity;
-			 session_write_close();
-
-			 header('Location: '.AT_BASE_HREF.'bounce.php?course='.$default_course_id);
-                         exit;
-                         
-                     }
+                     registerAndLogin($openid, $openid_fname, $openid_lname, $openid_email, $openid_country);                     
                  }else{
                      #User has already registered. It's time to login.
-                     
-                     #Garbage collect for maximum login attempts table
-                     if (rand(1, 100) == 1){
-                        $sql = 'DELETE FROM '.TABLE_PREFIX.'member_login_attempt WHERE expiry < '. time();
-                        mysql_query($sql, $db);
-                     }
-                     
-                     if (version_compare(PHP_VERSION, '5.1.0', '>=')) {
-                        session_regenerate_id(TRUE);
-                     }
-                     
-                     #Get the login name, member id, status and preferences.
-                     list($m_id, $login_name, $status, $preferences, $language, $last_login) = mysql_fetch_array($result);
-                     
-                     #Check if this account has exceeded maximum attempts
-                     $sql = 'SELECT login, attempt, expiry FROM '.TABLE_PREFIX."member_login_attempt WHERE login='$login_name'";
-
-                     $result = mysql_query($sql, $db);
-	             if ($result && mysql_numrows($result) > 0){
-                         list($attempt_login_name, $attempt_login, $attempt_expiry) = mysql_fetch_array($result);
-                     } else {
-                         $attempt_login_name = '';
-                         $attempt_login = 0;
-                         $attempt_expiry = 0;
-                     }
-                     
-                     if($attempt_expiry > 0 && $attempt_expiry < time()){
-                        
-                         #Clear entry if it has expired
-                         $sql = 'DELETE FROM '.TABLE_PREFIX."member_login_attempt WHERE login='$login_name'";
-                         mysql_query($sql, $db);
-                         $attempt_login = 0;	
-                         $attempt_expiry = 0;
-                     }
-                     
-                     if($_config['max_login'] > 0 && $attempt_login >= $_config['max_login']){
-                         $msg->addError('MAX_LOGIN_ATTEMPT');
-                         header('Location: ' .OPENID_MOD_DIR.'openid_login.php');
-                         exit;
-                     } else if ($status == AT_STATUS_UNCONFIRMED) {
-                         $msg->addError('NOT_CONFIRMED');
-                         #TODO: Make the mail id as conformed.
-                         header('Location: ' .OPENID_MOD_DIR.'openid_login.php');
-                         exit;
-                     } else if ($status == AT_STATUS_DISABLED) {
-                         $msg->addError('ACCOUNT_DISABLED');
-                         header('Location: ' .OPENID_MOD_DIR.'openid_login.php');
-                         exit;
-                     }else{
-                         
-                         #Everythings OK. Make Login.
-                         $_SESSION['valid_user'] = true;
-                         $_SESSION['member_id']  = intval($m_id);
-                         $_SESSION['login']	 = $login_name;
-                         if ($preferences == "")
-                            assign_session_prefs(unserialize(stripslashes($_config["pref_defaults"])), 1);
-                         else
-                            assign_session_prefs(unserialize(stripslashes($preferences)), 1);
-                         $_SESSION['is_guest']	= 0;
-                         $_SESSION['lang']	= $language;
-                         $_SESSION['course_id']  = 0;
-                         $now = date('Y-m-d H:i:s');
-                         $default_course_id = 0;
-                         
-                         #Its an OpenID session
-                         $_SESSION['is_openid'] = true;
-                         $_SESSION['openid_claimed_id'] = $openid->identity;
-                         
-                         $_SESSION['first_login'] = false;
-                         if ($last_login == null || $last_login == '' 
-                            || $last_login == '0000-00-00 00:00:00' 
-                            || $_SESSION['prefs']['PREF_MODIFIED']!==1) {
-                                
-                             $_SESSION['first_login'] = true;
-                         }
-
-                         $sql = "UPDATE ".TABLE_PREFIX."members SET creation_date=creation_date, last_login='$now' WHERE member_id=$_SESSION[member_id]";
-                         mysql_query($sql, $db);
-
-                         #Clear login attempt on successful login
-                         $sql = 'DELETE FROM '.TABLE_PREFIX."member_login_attempt WHERE login='$login_name'";
-                         mysql_query($sql, $db);
-		
-                         $msg->addFeedback('LOGIN_SUCCESS');
-                            
-                         header('Location: '.AT_BASE_HREF.'bounce.php?course='.$default_course_id);
-                         exit;
-                    }
+                     makeLogin( $result, $openid);
                  }
                  
                  #User has failed to login. Unset the session.
