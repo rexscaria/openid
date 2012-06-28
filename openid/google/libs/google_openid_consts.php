69ede162b17492098b11c9eee6e273e28b40d1d3
Arun Scaria <arunscaria91@gmail.com>
2012-06-28 23:54:57 +0530

    google consts

---
+++ openid/google/libs/google_openid_consts.php
@@ -0,0 +1,49 @@
+<?php
+
+/* * ******************************************************************** */
+/* ATutor							       */
+/* * ******************************************************************** */
+/* Copyright (c) 2002-2012                                             */
+/* Inclusive Design Institute	                                       */
+/* http://atutor.ca                                                    */
+/*                                      			       */
+/* This program is free software. You can redistribute it and/or       */
+/* modify it under the terms of the GNU General Public License         */
+/* as published by the Free Software Foundation.		       */
+/* * ******************************************************************** */
+// $Id: google_openid_consts.php UTF-8 10055 Jun 26, 2012 3:25:59 AM Author:scari  $
+
+
+#OAuth namespace.
+define(OAUTH_EXT2_NAMESPACE, 'http://specs.openid.net/extensions/oauth/1.0');
+
+#Scopes-Google OAuth.
+define(OAUTH_SCOPE_GOOGLE_ANALATICS,       'https://www.google.com/analytics/feeds/');
+define(OAUTH_SCOPE_GOOGLE_BASE,            'https://www.google.com/base/feeds/');
+define(OAUTH_SCOPE_GOOGLE_BUZZ,            'https://www.googleapis.com/auth/buzz');
+define(OAUTH_SCOPE_GOOGLE_BOOKS,           'https://www.google.com/books/feeds/');
+define(OAUTH_SCOPE_GOOGLE_BLOGGER,         'https://www.blogger.com/feeds/');
+define(OAUTH_SCOPE_GOOGLE_CALANDER,        'https://www.google.com/calendar/feeds/');
+define(OAUTH_SCOPE_GOOGLE_CONTACTS,        'https://www.google.com/m8/feeds/');
+define(OAUTH_SCOPE_GOOGLE_CHROME_WEBSTORE, 'https://www.googleapis.com/auth/chromewebstore.readonly');
+define(OAUTH_SCOPE_GOOGLE_DOCS,            'https://docs.google.com/feeds/');
+define(OAUTH_SCOPE_GOOGLE_FINANCE,         'https://finance.google.com/finance/feeds/');
+define(OAUTH_SCOPE_GOOGLE_MAIL,            'https://mail.google.com/mail/feed/atom');
+define(OAUTH_SCOPE_GOOGLE_HEALTH,          'https://www.google.com/health/feeds/');
+define(OAUTH_SCOPE_GOOGLE_H9,              'https://www.google.com/h9/feeds/');
+define(OAUTH_SCOPE_GOOGLE_MAPS,            'https://maps.google.com/maps/feeds/');
+define(OAUTH_SCOPE_GOOGLE_MODERATOR,       'https://www.googleapis.com/auth/moderator');
+define(OAUTH_SCOPE_GOOGLE_OPEN_SOCIAL,     'https://www-opensocial.googleusercontent.com/api/people/');
+define(OAUTH_SCOPE_GOOGLE_ORKUT,           'https://orkut.gmodules.com/social/rest');
+define(OAUTH_SCOPE_GOOGLE_PICASA,          'https://picasaweb.google.com/data/');
+define(OAUTH_SCOPE_GOOGLE_SSIDEWIKI,       'https://www.google.com/sidewiki/feeds/');
+define(OAUTH_SCOPE_GOOGLE_SITES,           'https://sites.google.com/feeds/');
+define(OAUTH_SCOPE_GOOGLE_SPREADSHEETS,    'https://spreadsheets.google.com/feeds/');
+define(OAUTH_SCOPE_GOOGLE_TASKS,           'https://www.googleapis.com/auth/tasks');
+define(OAUTH_SCOPE_GOOGLE_URL_SHORTNER,    'https://www.googleapis.com/auth/urlshortener');
+define(OAUTH_SCOPE_GOOGLE_WAVE,            'http://wave.googleusercontent.com/api/rpc');
+define(OAUTH_SCOPE_GOOGLE_WEBMASTER_TOOLS, 'https://www.google.com/webmasters/tools/feeds/');
+define(OAUTH_SCOPE_GOOGLE_YOUTUBE,         'https://gdata.youtube.com');
+
+
+?>
