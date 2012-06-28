3ea3ae65e4246851bf1a9acec160032009a5b7cc
Arun Scaria <arunscaria91@gmail.com>
2012-06-29 00:09:58 +0530

    added twitter classess

---
+++ openid/twitter/libs/twitter_openid_consts.php
@@ -0,0 +1,15 @@
+<?php
+
+/* * ******************************************************************** */
+/* ATutor							       */
+/* * ******************************************************************** */
+/* Copyright (c) 2002-2012                                             */
+/* Inclusive Design Institute	                                       */
+/* http://atutor.ca                                                    */
+/*                                      			       */
+/* This program is free software. You can redistribute it and/or       */
+/* modify it under the terms of the GNU General Public License         */
+/* as published by the Free Software Foundation.		       */
+/* * ******************************************************************** */
+// $Id: twitter_openid_consts.php UTF-8 10055 Jun 26, 2012 10:01:07 PM Author:scari  $
+?>
